d5a410455571712e352a59f410eb0faafd72c0d8
TellSamm <chernikov.semen21@gmail.com>
2022-07-27 12:50:51 +0300

    Initial commit

---
+++ src/test/java/lessonOne.java
@@ -0,0 +1,36 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.Driver;
+
+public class lessonOne {
+
+
+
+
+    @Test
+    public void lessonOne(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Семён\\Desktop\\Automatization_Test\\chromedriver_2.exe");
+        WebDriver driverNew = new ChromeDriver();
+
+        driverNew.get("https://grinfer.com/");
+        driverNew.manage().window().maximize();
+
+        String getTitle = driverNew.getTitle();
+        Assert.assertEquals(getTitle, "Top Online Courses | Grinfer"); // перевод: Утверждать. утверждать равно
+
+        String gerUrl = driverNew.getCurrentUrl();
+        Assert.assertEquals(gerUrl, "https://grinfer.com/");
+
+
+        String getPage = driverNew.getPageSource();// метод для вывода всего Html кода страницы
+        System.out.println("!!Каша просто каша по городу яхай !!получилось!!" + getPage); // выводим этот html код
+        driverNew.close();
+
+
+
+
+    }
+}
