669a8e8c316ae2dfc44d5c8ec2fb662d96db2e47
TellSamm <chernikov.semen21@gmail.com>
2022-07-27 13:45:19 +0300

    циклы обучение

---
+++ src/test/java/Test_1.java
@@ -0,0 +1,22 @@
+public class Test_1 {
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    int a [] = new int[100];
+
+    for (int i = 0; i < a.length; i++){
+        System.out.println(a[i]);
+    }
+
+    for (int y: a){
+        System.out.println("Тест прошёл успешно "  + y);
+    }
+
+
+    }
+}
