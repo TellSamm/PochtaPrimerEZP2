10576fa9a881005a7430b7d869de0c1eff9ab0c3
TellSamm <chernikov.semen21@gmail.com>
2022-08-09 22:17:36 +0300

    циклы обучение

---
+++ src/test/java/Praktikum.java
@@ -0,0 +1,21 @@
+public class Praktikum {
+    public static void main(String[] args) {
+        //public String getAdviceOnProcrastination(int dice) {
+        //String result = "тут будет храниться совет";
+        int dice = 5;
+        if (dice == 1) {
+            System.out.println("Полистать мемы");
+        } else if (dice == 2) {
+            System.out.println("Выпить чашечку чая");
+        } else if (dice == 3) {
+            System.out.println("Полистать ленту новостей");
+        } else if (dice == 4) {
+            System.out.println("Пообщаться с уточкой");
+        } else if (dice == 5) {
+            System.out.println("Устроить small talk");
+        } else if (dice == 6) {
+            System.out.println("Потыкать в носик котика или пёсика");
+        }
+        return ;
+    }
+}
