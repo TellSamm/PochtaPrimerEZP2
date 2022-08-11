cf4f5ddf262231f7eb8e8da485ffb366e88982f2
TellSamm <chernikov.semen21@gmail.com>
2022-08-11 17:48:14 +0300

    Обучение Yandex

---
+++ src/test/java/Praktikum_3.java
@@ -0,0 +1,13 @@
+public class Praktikum_3 {
+
+    public static void main(String[] args) {
+        int daysBeforeExam = 35; // Дни до старта сессии, цикл должен начинаться с их значения
+
+        for (int i = 35; i >= 0; i = i - 7) { // Напиши цикл
+            // Место для вывода
+            System.out.println("До первого экзамена осталось дней:" + i);
+        }
+
+        System.out.println("Сессия начинается. Халява, приди!");
+    }
+}
